7ecea6bdab397bb264f55a80a89a102b21bca872
mw-hrastega <48831250+mw-hrastega@users.noreply.github.com>
2020-09-02 15:24:12 -0400

    Update runMyTests.m

--- runMyTests.m
+++ runMyTests.m
@@ -1,15 +1,17 @@
-disp(matlabroot)
 import matlab.unittest.TestRunner
-import matlab.unittest.Verbosity
 import matlab.unittest.plugins.CodeCoveragePlugin
 import matlab.unittest.plugins.codecoverage.CoberturaFormat
  
-suite = testsuite(pwd, 'IncludeSubfolders', true);
+suite = testsuite(pwd,'IncludeSubfolders',true);
  
-runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
-runner.addPlugin(CodeCoveragePlugin.forFolder({'.'}, 'IncludingSubfolders', true, 'Producing', CoberturaFormat('coverage.xml')));
+runner = TestRunner.withTextOutput('OutputDetail',3);
  
-results = runner.run(suite);
+sourceFolder = pwd;
+reportFile = 'cobertura.xml';
+reportFormat = CoberturaFormat(reportFile);
+p = CodeCoveragePlugin.forFolder(sourceFolder,'IncludingSubfolders', true,'Producing',reportFormat);
+runner.addPlugin(p)
  
+results = runner.run(suite);  
 nfailed = nnz([results.Failed]);
-assert(nfailed == 0, [num2str(nfailed) ' test(s) failed.']);
+assert(nfailed == 0, [num2str(nfailed) ' test(s) failed.'])
