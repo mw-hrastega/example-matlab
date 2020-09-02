52a741f5b69f0c465d8fc24900c0b1f3b0561543
mw-hrastega <48831250+mw-hrastega@users.noreply.github.com>
2020-09-02 15:40:44 -0400

    Update runMyTests.m

--- runMyTests.m
+++ runMyTests.m
@@ -1,17 +1,22 @@
 import matlab.unittest.TestRunner
+import matlab.unittest.Verbosity
 import matlab.unittest.plugins.CodeCoveragePlugin
 import matlab.unittest.plugins.codecoverage.CoberturaFormat
  
+% Create a test suite 
 suite = testsuite(pwd,'IncludeSubfolders',true);
  
-runner = TestRunner.withTextOutput('OutputDetail',3);
- 
+% Create a test runner that displays test run progress at the matlab.unittest.Verbosity.Detailed level
+runner = TestRunner.withTextOutput('OutputDetail',Verbosity.Detailed); 
+
+% Create a CodeCoveragePlugin instance and add it to the test runner
 sourceFolder = pwd;
 reportFile = 'cobertura.xml';
 reportFormat = CoberturaFormat(reportFile);
 p = CodeCoveragePlugin.forFolder(sourceFolder,'IncludingSubfolders', true,'Producing',reportFormat);
 runner.addPlugin(p)
  
+% Run the tests and fail the build if any of the tests fails 
 results = runner.run(suite);  
 nfailed = nnz([results.Failed]);
 assert(nfailed == 0, [num2str(nfailed) ' test(s) failed.'])
